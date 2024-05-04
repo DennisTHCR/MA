b1841a89bd1dab1bbac1aa16488bb952aca29da9
DennisTHCR <dennis.winghardtdd@gmail.com>
2024-05-04 17:09:31 +0200

    remember i'm using ecs, not oop

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -1,14 +1,12 @@
 pub mod camera;
-pub mod resources;
 
 use bevy::prelude::*;
 use camera::CameraPlugin;
-use resources::ResourcesPlugin;
 
 pub struct GraphicsPlugin;
 
 impl Plugin for GraphicsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((CameraPlugin, ResourcesPlugin));
+        app.add_plugins((CameraPlugin,));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 mod graphics;
 mod utilities;
-mod scene_management;
 
 use bevy::prelude::*;
 use bevy_inspector_egui::bevy_egui::EguiContexts;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use graphics::GraphicsPlugin;
-use scene_management::SceneManagementPlugin;
 
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins.set(ImagePlugin::default_nearest()),
             GraphicsPlugin,
-            SceneManagementPlugin,
             WorldInspectorPlugin::new(),
         ))
         .add_systems(Startup, (setup, sprite_test).chain())
